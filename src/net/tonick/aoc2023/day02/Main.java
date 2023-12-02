15779459983ce58917a3851f90c09ba305cd4eb1
Oli Thissen <oli@tonick.net>
2023-12-02 09:02:24 +0100

    Solves Day 02

--- src/net/tonick/aoc2023/day02/Main.java
+++ src/net/tonick/aoc2023/day02/Main.java
@@ -3,42 +3,93 @@ package net.tonick.aoc2023.day02;
 import net.tonick.aoc2023.util.InputFile;
 import net.tonick.aoc2023.util.Solver;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main extends Solver {
+    private record Game(int id, List<Draw> draws) {
+
+    }
+    private record Draw(int red, int green, int blue) {
+
+    }
+
+    private static final Function<String, Draw> createDraw = line -> {
+        Map<String, Integer> x = Arrays.stream(line.split(","))
+                .map(String::trim)
+                .map(s -> s.split(" "))
+                .collect(Collectors.toMap(strings -> strings[1], strings -> Integer.valueOf(strings[0])));
+        return new Draw(
+                Optional.ofNullable(x.get("red")).orElse(0),
+                Optional.ofNullable(x.get("green")).orElse(0),
+                Optional.ofNullable(x.get("blue")).orElse(0));
+    };
+
+    private static final Function<String, List<Draw>> createDraws = line -> {
+        var draws = line.substring(line.indexOf(":") + 1).split(";");
+        var x = Arrays.stream(draws).map(createDraw).toList();
+        return x;
+    };
+
+    private static final Function<String, Game> createGame = line -> {
+        var gameId = line.substring(line.indexOf(" ") + 1, line.indexOf(":"));
+        var draws = createDraws.apply(line);
+        return new Game(Integer.parseInt(gameId), draws);
+    };
+
+    private static final Predicate<Game> validateGame = game -> game.draws.stream().allMatch(draw -> draw.red <= 12 && draw.green <= 13 && draw.blue <= 14);
+
+    private static final Function<Game, Draw> createMetaDraw = game -> {
+        var maxRed = game.draws.stream().mapToInt(d -> d.red).max().orElse(0);
+        var maxGreen = game.draws.stream().mapToInt(d -> d.green).max().orElse(0);
+        var maxBlue = game.draws.stream().mapToInt(d -> d.blue).max().orElse(0);
+
+        return new Draw(maxRed, maxGreen, maxBlue);
+    };
+
+    private static final ToIntFunction<Draw> calculateChecksum = draw -> draw.red * draw.green * draw.blue;
+
     /**
      * Solution 1: Apply the 'extractOnlyByDigits' extractor to the input list and sum up the results
      */
-    private static final Function<List<String>, Object> solution1 = input -> input.stream()
-            .findAny();
+    private static final Function<List<String>, Object> solution1 = input -> {
+        return input.stream()
+                .map(createGame)
+                .filter(validateGame)
+                .mapToInt(game -> game.id)
+                .sum();
+    };
 
-    private static final Function<List<String>, Object> solution2 = input -> input.stream()
-            .findAny();
+    private static final Function<List<String>, Object> solution2 = input -> {
+        return input.stream()
+                .map(createGame)
+                .map(createMetaDraw)
+                .mapToInt(calculateChecksum)
+                .sum();
+    };
 
 
     public static void main(String... args) {
         Solver.solve(
                 InputFile.of(Main.class, "sample.txt"),
                 solution1,
-                Optional.empty()
+                Optional.of(8)
         );
 
         Solver.solve(
                 InputFile.of(Main.class, "input.txt"),
                 solution1,
-                Optional.empty()
+                Optional.of(2476)
         );
 
         Solver.solve(
-                InputFile.of(Main.class, "sample2.txt"),
+                InputFile.of(Main.class, "sample.txt"),
                 solution2,
-                Optional.empty()
+                Optional.of(2286)
         );
 
         Solver.solve(
